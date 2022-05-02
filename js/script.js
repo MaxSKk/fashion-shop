23a326fecfcfe967f129e76ae7502e9917927b43
MaxSk <maxskokov4@gmail.com>
2022-05-02 20:18:49 +0300

    add catalog page

--- js/script.js
+++ js/script.js
@@ -8,4 +8,17 @@ $(document).ready(function () {
       $('.header__burger, .menu, .menu__closer').toggleClass('active');
       $('body').toggleClass('lock');
    });
+
+   $('.spoiler__title').click(function (event) {
+      $(this).toggleClass('open').next().slideToggle(300);
+   });
+
+   $('.filter__title').click(function (event) {
+      $(this).toggleClass('open').next().slideToggle(300);
+      $('.filter').toggleClass('open');
+   });
+
+   $('.subfilter__title').click(function (event) {
+      $(this).toggleClass('open').next().slideToggle(300);
+   });
 });
